9ac54acde9dcf1f4f58a6b2fa089f43217ee17da
Louwrentius <louwrentius@gmail.com>
2010-04-11 12:46:22 +0000

    backup, improved init script, install script
    git-svn-id: http://wanfailoverscript.googlecode.com/svn/trunk@13 66989f9f-ffbd-eb33-a4e1-7d92d0df9447

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+BINARY=wfs
+CONFIG=wfs.conf
+INITSCRIPT=init.d/wfs
+
+BINARY_TARGET=/usr/bin
+INITSCRIPT_TARGET=/etc/init.d
+CONFIG_TARGET=/etc
+
+if [ "$1" == "remove" ]
+then
+    if [ -e /etc/debian_version ]
+    then
+        update-rc.d -f wfs remove
+    elif [ -e /etc/redhat-release ]
+    then
+        chkconfig --del wfs
+    fi
+
+    rm $BINARY_TARGET/$BINARY
+    rm $CONFIG_TARGET/$CONFIG
+    rm $INITSCRIPT_TARGET/`basename $INITSCRIPT`
+    exit
+fi
+
+cp $BINARY $BINARY_TARGET
+cp $INITSCRIPT $INITSCRIPT_TARGET
+
+if [ -e $CONFIG_TARGET/$CONFIG ]
+then
+    echo "Existing configuration found. Creating $CONFIG_TARGET/$CONFIG.new"
+    echo "Update your existing configuration file with the new one or WFS may"
+    echo "not operate properly due to changes."
+    cp $CONFIG $CONFIG_TARGET/$CONFIG.new
+else
+    cp $CONFIG $CONFIG_TARGET/$CONFIG
+fi
+
+if [ -e /etc/debian_version ]
+then
+    update-rc.d wfs defaults 99 10
+elif [ -e /etc/redhat-release ]
+then
+    chkconfig --levels 2345 wfs on
+fi
+
+
+
